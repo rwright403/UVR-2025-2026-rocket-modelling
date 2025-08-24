0c5ba9fc60036c2297344cfea7aec936280b536a
rwright <rwright2500@gmail.com>
2025-08-23 20:06:33 -0700

    23-aug-2025 idk what i was doing before works now

--- rocketpy_deployable_payloadexample/rocketpy_two_stage.py
+++ rocketpy_deployable_payloadexample/rocketpy_two_stage.py
@@ -1,7 +1,9 @@
+import numpy as np
+
 from rocketpy import Environment, SolidMotor, Rocket, Flight
 import datetime
 
-#Using info from Caltech's AJAKS rocket to learn rocketpy for 2 stages
+#Using info roughly inspired by from Caltech's AJAKS rocket to learn rocketpy for 2 stages
 
 
 
@@ -11,8 +13,12 @@ tomorrow = datetime.date.today() + datetime.timedelta(days=1)
 env.set_date(
     (tomorrow.year, tomorrow.month, tomorrow.day, 12)
 )  # Hour given in UTC time
-env.set_atmospheric_model(type="Forecast", file="GFS")
-env.max_expected_height = 8000
+# Force standard atmosphere (ISA) with NO wind
+env.set_atmospheric_model(type="standard_atmosphere")
+
+# Explicitly zero wind everywhere
+env.wind_speed = 0 
+
 
 
 
@@ -40,7 +46,7 @@ Pro75M1670 = SolidMotor(
 
 # 14.426 is the mass of the rocket including the payload but without the motor
 payload_mass = 4.5  # in kg
-rocket_mass = 14.426 - payload_mass  # in kg
+rocket_mass = 40 - payload_mass  # in kg
 
 
 stage1_rocket = Rocket(
@@ -72,37 +78,35 @@ stage1_rocket.add_tail(
 
 #lower stage 1 fins
 stage1_rocket.add_trapezoidal_fins(
-    n=5,
+    n=4,
     root_chord=0.25,       # [m] from figure
     tip_chord=0.10,        # [m]
-    span=0.15,            # [m]
-    position=3.9243     # [m] fin root LE to CM
+    span=0.1,            # [m]
+    position=5.03174    # [m] fin root LE to CM
 )
 
 #upper stage 2 fins!
 stage1_rocket.add_trapezoidal_fins(
-    n=5,
+    n=3,
     root_chord=0.25,       # [m] from figure
     tip_chord=0.10,        # [m]
-    span=0.15,            # [m]
+    span=0.1,            # [m]
     position=2.25     # [m] fin root LE to CM
 )
 
 
-stage1_rocket.draw()
+#stage1_rocket.draw()
 
 stage1_flight = Flight(
     rocket=stage1_rocket,
     environment=env,
-    rail_length=10.2,
-    inclination=85,
-    heading=25,
+    rail_length=20.2,
+    inclination=89,
+    heading=0,
     terminate_on_apogee=True,
     name="stage1_flight",
 )
 
-
-print("here")
 
 ### 2. Stage 2 Definition
 
@@ -112,7 +116,7 @@ stage2_rocket = Rocket(
     inertia=(2.321, 2.321, 0.034),
     power_off_drag="./powerOffDragCurve.csv",
     power_on_drag="./powerOnDragCurve.csv",
-    center_of_mass_without_motor=1.21412,
+    center_of_mass_without_motor=1.4,
     coordinate_system_orientation="nose_to_tail",
 )
 
@@ -138,29 +142,34 @@ Pro75M1670upper = SolidMotor(
 stage2_rocket.add_motor(Pro75M1670upper, position=2.6035)
 
 stage2_rocket.add_trapezoidal_fins(
-    n=5,
+    n=3,
     root_chord=0.25,       # [m] from figure
     tip_chord=0.10,        # [m]
-    span=0.15,            # [m]
+    span=0.1,            # [m]
     position=2.25     # [m] fin root LE to CM
 )
 
 
 stage2_rocket.add_nose(length=0.3048, kind="von karman", position=0)
 
-stage2_rocket.draw()
+#stage2_rocket.draw()
 
 
 ### sim
 # sol vector: [t, x, y, z, vx, vy, vz, e0, e1, e2, e3, w1, w2, w3]
-new_sol = stage1_flight.solution[-1]
-new_sol[0] = 2.0 #reset initial time
-new_sol[7:11] = [1.0, 0.0, 0.0, 0.0]  # e0, e1, e2, e3
+new_sol = stage1_flight.get_solution_at_time(3, atol=0.001) #stage1_flight.solution[-1].copy()
+vel = new_sol[4:7] # [vx, vy, vz]
 
-print("initial solution:\n")
-for i in new_sol:
+print("vec:\n")
+for i in vel:
     print(i)
 
+
+
+"""print("initial solution:\n")
+for i in new_sol:
+    print(i)
+"""
 
 stage2_flight = Flight(
     rocket=stage2_rocket,
@@ -172,7 +181,7 @@ stage2_flight = Flight(
     name="stage2_flight",
 )
 
-#stage2_flight.all_info()
+#stage1_flight.all_info()
 
 
 from rocketpy.plots.compare import CompareFlights
